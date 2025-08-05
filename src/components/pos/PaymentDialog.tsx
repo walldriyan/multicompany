13831eba69c063f11b5ac9102febb389a280f452
shehan gamage <shehan.gamage2@gmail.com>
2025-08-05 11:35:26 +0000

    me red rounm karala tiyenne amatara eda, browser eke pront dialogbox eke

--- src/components/pos/PaymentDialog.tsx
+++ src/components/pos/PaymentDialog.tsx
@@ -289,8 +289,9 @@ export function PaymentFormContent({
               const printHtml = `
                   <html><head><title>Print Bill - ${billNumber}</title>
                   <style>
-                      body { margin: 0; font-family: 'Courier New', Courier, monospace; font-size: 8pt; background-color: white; color: black; }
-                      .receipt-container { width: 280px; margin: 0 auto; padding: 5px; height: auto; } 
+                      @page { size: auto;  margin: 0mm; }
+                      body { margin: 0; font-family: 'Courier New', Courier, monospace; font-size: 8pt; background-color: white; color: black; height: fit-content; }
+                      .receipt-container { width: 280px; margin: 0 auto; padding: 5px; height: fit-content; } 
                       table { width: 100%; border-collapse: collapse; font-size: 7pt; margin-bottom: 3px; }
                       th, td { padding: 1px 2px; vertical-align: top; font-size: 7pt; } .text-left { text-align: left; } .text-right { text-align: right; } .text-center { text-align: center; }
                       .font-bold { font-weight: bold; } .company-details p, .header-info p, .customer-name { margin: 0px 0; line-height: 1.1; font-size: 8pt; }
@@ -303,7 +304,7 @@ export function PaymentFormContent({
                       .discount-details { font-size: 7pt; margin-left: 5px; margin-top: 1px; margin-bottom: 1px; }
                       .discount-details div { display: flex; justify-content: space-between; } .discount-details span:first-child { padding-right: 3px; }
                       th { font-size: 7pt; white-space: normal; text-align: right; } th.item-name { text-align: left; }
-                      @media print { body { -webkit-print-color-adjust: exact; print-color-adjust: exact; font-size: 8pt !important; color: black !important; background-color: white !important;} .receipt-container { margin: 0; padding:0; width: 100%; height: auto !important; } table { font-size: 7pt !important; } }
+                      @media print { body { -webkit-print-color-adjust: exact; print-color-adjust: exact; font-size: 8pt !important; color: black !important; background-color: white !important; height: auto !important; } .receipt-container { margin: 0; padding:0; width: 100%; height: fit-content !important; } table { font-size: 7pt !important; } }
                   </style></head><body><div class="receipt-container">${printContents}</div></body></html>
               `;
               doc.write(printHtml); doc.close();
